ffc682d23dce5e65d40e3d234e8555cf188537cd
Dak <dakore879@gmail.com>
2025-02-04 00:59:38 +0100

    remove useless protection

--- philo/srcs/philo.c
+++ philo/srcs/philo.c
@@ -6,7 +6,7 @@
 /*   By: rsebasti <rsebasti@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 10:57:17 by rsebasti          #+#    #+#             */
-/*   Updated: 2025/02/04 00:57:45 by rsebasti         ###   ########.fr       */
+/*   Updated: 2025/02/04 00:59:22 by rsebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -99,8 +99,6 @@ int	main(int argc, char **argv)
 			putstr_fd(2, " <time_to_sleep> [eat_x_time]\n"), 0);
 	init_var(&table, argc, argv);
 	endgame(&table);
-	sleep_ms(table.time_eat);
-	usleep(5000);
 	free_table(&table);
 	return (1);
 }
